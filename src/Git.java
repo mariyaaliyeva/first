89fd02b277ecf7393817945169f933ce00009c1c
Mariya Aliyeva <mariyaalyevaseo@gmail.com>
2022-04-14 12:16:55 +0600

    Удалила все

--- src/Git.java
+++ src/Git.java
@@ -1,11 +1,7 @@
 public class Git {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        int sum = a + b;
-        int x = b-a;
-        int u = b/a;
-        System.out.println(sum);
-        System.out.println(x);
+        for (char c = 9398; c < 10179; c++) {
+            System.out.print(c);
+        }
     }
 }
